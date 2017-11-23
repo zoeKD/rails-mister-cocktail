be8d7bff256a370935f115b49d4e30199cff14da
Zoe Cade <zoe.cade@gmail.com>
2017-11-23 16:03:51 +0100

    most of the backend

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,25 @@
+class DosesController < ApplicationController
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
